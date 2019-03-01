243a1919caa3504b12f82714238caa1472fb5a4d
kangxuanxin <602095770@qq.com>
2019-03-01 16:00:09 +0800

    基本完成脚手架

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,28 @@
-import React, { Component } from "react"
-import { Button } from "antd"
+import React from "react"
+import { connect } from "react-redux"
 
-export default class Home extends Component {
-  componentDidMount = () => {
-    console.log(2233)
+import { loadData, startClock, tickClock } from "../store/actions"
+import Page from "../components/page"
+
+class Index extends React.Component {
+  static async getInitialProps(props) {
+    const { store, isServer } = props.ctx
+    store.dispatch(tickClock(isServer))
+
+    if (!store.getState().placeholderData) {
+      store.dispatch(loadData())
+    }
+
+    return { isServer }
+  }
+
+  componentDidMount() {
+    this.props.dispatch(startClock())
   }
 
   render() {
-    return (
-      <div>
-        welcome to next.js
-        <Button>antd</Button>
-      </div>
-    )
+    return <Page title="Index Page" linkTo="/other" NavigateTo="Other Page" />
   }
 }
+
+export default connect()(Index)
