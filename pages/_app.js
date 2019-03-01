866557ccac85eb7177ec0519eb826c937f3947c6
kangxuanxin <602095770@qq.com>
2019-03-01 16:00:31 +0800

    格式化代码

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,13 @@
-import App, { Container } from 'next/app'
-import React from 'react'
-import { Provider } from 'react-redux'
-import withRedux from 'next-redux-wrapper'
-import withReduxSaga from 'next-redux-saga'
+import App, { Container } from "next/app"
+import React from "react"
+import { Provider } from "react-redux"
+import withRedux from "next-redux-wrapper"
+import withReduxSaga from "next-redux-saga"
 
-import createStore from '../store/store'
+import createStore from "../store/store"
 
 class MyApp extends App {
-  static async getInitialProps ({ Component, ctx }) {
+  static async getInitialProps({ Component, ctx }) {
     let pageProps = {}
 
     if (Component.getInitialProps) {
@@ -17,7 +17,7 @@ class MyApp extends App {
     return { pageProps }
   }
 
-  render () {
+  render() {
     const { Component, pageProps, store } = this.props
     return (
       <Container>
